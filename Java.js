11ad36abbeab5049e1bde694f11a34f128b9d011
AnthonyDaniel37 <80599284+AnthonyDaniel37@users.noreply.github.com>
2021-05-18 16:52:59 -0300

    Enxurrada de Bits-Java Script
    Bom demais mais ainda tenho que pegar o jeito.

---
+++ Java.js
@@ -0,0 +1,9 @@
+const ano = 2021
+let datadenascimento
+let nome;
+let idade;
+
+datadenascimento = prompt ('Digite o ano em que você nasceu ')
+nome = prompt ('informe seu nome:')
+idade = ano - datadenascimento;
+alert("Olá " +nome + ' você tem ' +idade + " anos " );
